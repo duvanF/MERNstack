8221bb873101a7621d859917dc9bbdf523119d8c
duvanF <duvanf.pulgarin@gmail.com>
2020-01-16 00:12:53 -0500

    2 change
    backend - finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path')
+
+const { mongoose } = require('./database');
+
 const app = express();
 
-app.listen(3000, () => {
-    console.log('server on port 3000');
+//settings
+app.set('port',process.env.PORT || 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//routes
+app.use('/api/tasks' ,require('./routes/task.routes'));
+
+//statics files
+
+// console.log(path.join(__dirname, 'public'));
+// console.log(__dirname + '/public');
+app.use(express.static(path.join(__dirname, 'public')));
+
+//starting server
+app.listen(app.get('port'), () => {
+    console.log(`server on port ${app.get('port')}`);
 });
