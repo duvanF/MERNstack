c7c5f7d4b8451376832e3df0c569dbff9c8ca1b8
duvanF <duvanf.pulgarin@gmail.com>
2020-01-16 23:22:11 -0500

    9 change
    Frontend finished

--- src/app/App.js
+++ src/app/App.js
@@ -7,28 +7,51 @@ class App extends Component {
         super();
         this.state = {
             title: '',
-            description: ''
+            description: '',
+            tasks: [],
+            _id: ''
         };
         this.addTask = this.addTask.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     addTask(e) {
-        fetch('/api/tasks', {
-            method: 'POST',
-            body: JSON.stringify(this.state),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        })
+        if(this.state._id) {
+            fetch(`/api/tasks/${this.state._id}`, {
+                method: 'PUT',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
             .then(res => res.json())
             .then(data => {
-                console.log(data)
-                M.toast({html: 'Task Saved'});
-                this.setState({title: '', description: ''});
+                console.log(data);
+                M.toast({html: 'Task Updated'});
+                this.setState({title: '', description: '', _id: ''});
+                this.fetchTask();
+            });
+
+        } else {
+            fetch('/api/tasks', {
+                method: 'POST',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
             })
-            .catch(err => console.error(err));
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    M.toast({html: 'Task Saved'});
+                    this.setState({title: '', description: ''});
+                    this.fetchTask();
+                })
+                .catch(err => console.error(err));
+
+        }
 
         e.preventDefault();
     }
@@ -40,7 +63,41 @@ class App extends Component {
     fetchTask() {
         fetch('/api/tasks')
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(data => {
+                this.setState({tasks: data});
+                console.log(this.state.tasks); 
+            });
+    }
+
+    deleteTask(id) {
+        if(confirm('Are you sure that you want to delete the task?')) {
+            fetch(`/api/tasks/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                M.toast({html: 'Task Deleted'});
+                this.fetchTask();
+            });
+        }
+    }
+
+    updateTask(id) {
+        fetch(`/api/tasks/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                this.setState({
+                    title: data.title,
+                    description: data.description,
+                    _id: data._id
+                })
+            });
     }
 
     handleChange(e){
@@ -82,8 +139,37 @@ class App extends Component {
                             </div>
                         </div>
                         <div className="col s7">
-                            
-                            </div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                       <th>Title</th> 
+                                       <th>Description</th>
+                                       <th>Date</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.tasks.map(task => {
+                                            return (
+                                                <tr key={task._id}>
+                                                    <td>{task.title}</td>
+                                                    <td>{task.description}</td>
+                                                    <td>{task.date}</td>
+                                                    <td>
+                                                        <button className="bnt light-blue darken-4" onClick={() => this.updateTask(task._id)}>
+                                                            <i className="material-icons">edit</i>
+                                                        </button>
+                                                        <button className="bnt light-blue darken-4" onClick={() => this.deleteTask(task._id)}>
+                                                        <i className="material-icons">delete</i>
+                                                        </button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
